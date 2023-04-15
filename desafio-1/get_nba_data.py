5f5ac6670687817067e8c79cf5b2a32d1200245f
eliza-wollinger <wollingereliza@gmail.com>
2023-04-14 21:00:38 -0300

    update scripts

--- desafio-1/get_nba_data.py
+++ desafio-1/get_nba_data.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from psycopg2.extensions import AsIs
 
 
-def read_players_data(file_path: str) -> List[Dict]:
+def read_playroll_data(file_path: str) -> List[Dict]:
     with open(file_path) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         return [
@@ -20,22 +20,55 @@ def read_players_data(file_path: str) -> List[Dict]:
         ]
 
 
-def read_teams_data(file_path: str) -> List[Dict]:
+def read_player_box_score_stats_data(file_path: str) -> List[Dict]:
     with open(file_path) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         return [
             {
-                "id": int(row["TeamId"]),
-                "abbreviation": row["abbreviation"],
-                "team_name": row["Team"],
-                "simple_name": row["Simple"],
-                "location": row["Location"],
+                "id": int(row["Id"]),
+                "name": row["Player"],
+                "height": int(row["height (in)"]),
+                "weight": int(row["weight (lbs)"]),
+                "college": row["college"],
+                "birthdate": datetime.strptime(row["birth_date"], "%Y-%m-%d"),
             }
             for row in csv_reader
         ]
 
 
-def create_players_table(conn):
+def read_player_stats_data(file_path: str) -> List[Dict]:
+    with open(file_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        return [
+            {
+                "id": int(row["Id"]),
+                "name": row["Player"],
+                "height": int(row["height (in)"]),
+                "weight": int(row["weight (lbs)"]),
+                "college": row["college"],
+                "birthdate": datetime.strptime(row["birth_date"], "%Y-%m-%d"),
+            }
+            for row in csv_reader
+        ]
+
+
+def read_salaries_data(file_path: str) -> List[Dict]:
+    with open(file_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        return [
+            {
+                "id": int(row["Id"]),
+                "name": row["Player"],
+                "height": int(row["height (in)"]),
+                "weight": int(row["weight (lbs)"]),
+                "college": row["college"],
+                "birthdate": datetime.strptime(row["birth_date"], "%Y-%m-%d"),
+            }
+            for row in csv_reader
+        ]
+
+
+def create_nba_table(conn):
     with conn.cursor() as cur:
         cur.execute(
             """
@@ -53,24 +86,7 @@ def create_players_table(conn):
         conn.commit()
 
 
-def create_teams_table(conn):
-    with conn.cursor() as cur:
-        cur.execute(
-            """
-            CREATE TABLE IF NOT EXISTS teams (
-                id SERIAL PRIMARY KEY,
-                abbreviation VARCHAR(255) NOT NULL,
-                team_name VARCHAR(255) NOT NULL,
-                simple_name VARCHAR(255) NOT NULL,
-                location VARCHAR(255) NOT NULL,
-                created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-            )
-            """
-        )
-        conn.commit()
-
-
-def insert_players_data(conn, players_data: List[Dict]):
+def insert_nba_data(conn, players_data: List[Dict]):
     with conn.cursor() as cur:
         for player in players_data:
             cur.execute(
@@ -85,25 +101,6 @@ def insert_players_data(conn, players_data: List[Dict]):
                     player["weight"],
                     player["college"],
                     player["birthdate"],
-                ),
-            )
-        conn.commit()
-
-
-def insert_teams_data(conn, teams_data: List[Dict]):
-    with conn.cursor() as cur:
-        for team in teams_data:
-            cur.execute(
-                """
-                INSERT INTO teams
-                (abbreviation, team_name, simple_name, location)
-                VALUES (%s, %s, %s, %s)
-                """,
-                (
-                    team["abbreviation"],
-                    team["team_name"],
-                    team["simple_name"],
-                    team["location"],
                 ),
             )
         conn.commit()
